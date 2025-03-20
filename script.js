c5245dc66116055b506874c64c01e71d162d1a7d
Sadia Amjad <sadiaamjad518@gmail.com>
2025-03-20 09:27:41 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-const jokeContainer = document.getElementById("joke");
-const btn = document.getElementById("btn");
-// using joke api
-const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt&type=single";
-// using async and await
-const getJoke = async () => {
-    try {  //try 
-        jokeContainer.classList.remove("fade"); // Remove fade effect before updating joke
-
-        const response = await fetch(url);
-        const joke = await response.text();
-
-        jokeContainer.textContent = joke; // Display the joke
-        
-        setTimeout(() => {
-            jokeContainer.classList.add("fade"); // Apply fade effect after joke appears
-        }, 500); // Add a delay to ensure smooth transition
-    } catch (error) {   //shows error message
-        console.error("Error fetching joke:", error);
-        jokeContainer.textContent = "Oops! Something went wrong. 😢";
-    }
-};
-
-btn.addEventListener("click", getJoke);
-getJoke(); // Load a joke on page load
-
-
-
+// promise 1 resolve
+let promise = new Promise((resolve, reject) => {
+    alert("I am an alert in promise");
+    resolve(true);
+});
+console.log("Hello");
+setTimeout(function () {
+    console.log("Hello in 2 sec");
+}, 5000);
+console.log("My name is " + "Sadia");
+console.log(promise);
+// promise 2 reject
+let p = new Promise((resolve, reject) => {
+    console.log("Promise is pending");
+    setTimeout(() => {
+        console.log("Promise is rejected");
+        reject(new Error("I am an error"));
+    }, 5000);
+});
+console.log(p);
